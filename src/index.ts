e18f31583b4b3b014de7df00e79e87f78961af4a
Maryiamreda <maryiamreda@gmail.com>
2025-05-06 18:33:09 +0300

    get user receipts query

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,39 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { eq } from 'drizzle-orm';
 import { usersTable } from './db/schema';
 import postgres from 'postgres';
-
+import * as schema from './db/schema';
 const client = postgres(process.env.SUPABASE_URL!);
 const db = drizzle({ client });
 
-async function main() {
-    const user: typeof usersTable.$inferInsert = {
-        username: 'John',
-        age: 30,
-        email: 'john@example.com',
-    };
 
-    await db.insert(usersTable).values(user);
-    console.log('New user created!')
+async function getuserreceipts(userId: number){
+try{
+    const receipts = await db.select()
+    .from(schema.receiptTable).where(eq(schema.receiptTable.ownerId, userId));
+    return receipts;
 
-    const users = await db.select().from(usersTable);
-    console.log('Getting all users from the database: ', users)
+}catch(err){
+    console.error("Error fetching user receipts:", err);
+    throw err;
+
 
 }
+}
 
-main();
+
+// async function main() {
+//     const user: typeof usersTable.$inferInsert = {
+//         username: 'John',
+//         age: 30,
+//         email: 'john@example.com',
+//     };
+
+//     await db.insert(usersTable).values(user);
+//     console.log('New user created!')
+
+//     const users = await db.select().from(usersTable);
+//     console.log('Getting all users from the database: ', users)
+
+// }
+
+// main();
