bc80bc1364993d9337492c041c4c696c1c66fec0
Maryiamreda <maryiamreda@gmail.com>
2025-07-29 01:40:07 +0300

    get tags query

---
+++ src/server/backend/queries/tagsQueries.ts
@@ -0,0 +1,19 @@
+import { db } from "..";
+import * as schema from '../db/schema';
+import { eq ,or } from 'drizzle-orm';
+
+
+
+export async function getUserTags(UserId: number){
+try{
+      const tags = await db.select()
+            .from(schema.tagsTable).where( or(eq(schema.tagsTable.ownerId, UserId) ,     eq(schema.tagsTable.isEssential, true)
+ ) );
+            return{
+                data :tags
+            }
+}catch(error){
+    console.error("Error fetching tags:", error);
+    return { error: "Failed to fetch tags" };
+}
+}
